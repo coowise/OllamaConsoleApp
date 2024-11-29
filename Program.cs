266db0e7e5c1e45e7541dfc5478567a52da6e548
cw-matiastorres <matiastorres@coowise.com>
2024-11-29 16:49:54 -0300

    Initialize OllamaConsoleApp with ProcessStartInfo to execute 'ollama run llama3' command

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var process = new Process
+        {
+            StartInfo = new ProcessStartInfo
+            {
+                FileName = "ollama",
+                Arguments = "run llama3 'What is artificial intelligence?'",
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            }
+        };
+
+        process.Start();
+        string result = process.StandardOutput.ReadToEnd();
+        process.WaitForExit();
+
+        Console.WriteLine(result);
+    }
+}
